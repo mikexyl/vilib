fa1c77641a575f2a11845bcc2a9a2180634d0c24
Mike Xiangyu Liu <liu.xiangyu@ucy.ac.cy>
2025-09-10 15:05:33 +0300

    add benchmark example

--- visual_lib/test/images/create_feature_detector_evaluation_data.sh
+++ visual_lib/test/images/create_feature_detector_evaluation_data.sh
@@ -13,23 +13,6 @@ fi
 TARGET_FOLDER="euroc"
 BAG_NAME="MH_01_easy.bag"
 
-DO_DATASET_INITIALIZATION=1
-if [ ${DO_DATASET_INITIALIZATION} -eq 1 ]; then
-  rm -rf ${TARGET_FOLDER}
-  mkdir ${TARGET_FOLDER}
-  cd ${TARGET_FOLDER}
-
-  # Get dataset
-  read -p " Do you have the dataset already? [y/n] " ANSWER
-  if [ "${ANSWER}" != "y" ]; then
-    wget http://robotics.ethz.ch/~asl-datasets/ijrr_euroc_mav_dataset/machine_hall/MH_01_easy/MH_01_easy.bag
-  else
-    read -p " Where? Provide full path to the bag file, please. [path] " ANSWER
-    ln -s ${ANSWER} ${BAG_NAME}
-  fi
-else
-  cd ${TARGET_FOLDER}
-fi
 
 echo "[1] Dataset ready"
 read -p " Extract all images? [0 = all / N] " IMAGE_CNT_LIMIT
@@ -39,63 +22,6 @@ sleep 1
 # At this point we have either a symlink or file in the folder
 # Extract the images
 EXTRACT_PYTHON_SCRIPT_NAME="extract_python.py"
-cat << EOF > ${EXTRACT_PYTHON_SCRIPT_NAME}
-#!/usr/bin/python
-import rosbag
-import argparse
-import sys
-import os
-import cv2
-from cv_bridge import CvBridge
-
-parser = argparse.ArgumentParser(description='Extract left and right images from captured bags. The script upon request concatenates the left and right images for easier checking.')
-parser.add_argument('bag', metavar='bag', help='ROS bag file to be analyzed')
-parser.add_argument('output_dir', metavar='output_dir', help='Output directory')
-parser.add_argument('image_cnt', metavar='image_cnt', help='Number of images to be extracted')
-parsed = parser.parse_args();
-
-if (os.path.isfile(parsed.bag) == False):
-  print ('The specified file %s is not available' % parsed.bag)
-  print ('Please select an existing bag file');
-  sys.exit(-1);
-
-if (os.path.exists(parsed.output_dir) == True):
-  print ('The specified folder %s already exists' % parsed.output_dir);
-  print ('Please select a not existing directory.')
-  sys.exit(-1);
-else:
-  os.makedirs(parsed.output_dir);
-
-bridge = CvBridge();
-topic_to_extract = '/cam0/image_raw';
-output_sub_dir = '752_480';
-image_cnt_extracted = 0;
-image_cnt_limit = int(parsed.image_cnt);
-
-with rosbag.Bag(parsed.bag, 'r') as bag_in:
-  # Save topics individually first
-  if (os.path.exists(os.path.join(parsed.output_dir,output_sub_dir)) == False):
-    os.makedirs(os.path.join(parsed.output_dir,output_sub_dir));
-    img_base_path = os.path.join(parsed.output_dir,output_sub_dir);
-  print ('Topic information: %s\t|\t%d' % (topic_to_extract,bag_in.get_message_count(topic_to_extract)));
-  for topic, msg, t in bag_in.read_messages(topics=[topic_to_extract]):
-    # extract timestamp
-    nsec = (msg.header.stamp.secs*1e9) + msg.header.stamp.nsecs;
-    # extract Image data
-    cv_img = bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
-    cv2.imwrite(os.path.join(img_base_path, '%d.png' % nsec), cv_img)
-    print ('%d.png' % nsec)
-    image_cnt_extracted = image_cnt_extracted +1;
-    if(image_cnt_extracted == image_cnt_limit):
-      break;
-
-EOF
-chmod +x ${EXTRACT_PYTHON_SCRIPT_NAME}
-
-DO_EXTRACTION=1
-if [ ${DO_EXTRACTION} -eq 1 ]; then
-  ./${EXTRACT_PYTHON_SCRIPT_NAME} ${BAG_NAME} ./images ${IMAGE_CNT_LIMIT}
-fi
 
 echo "[2] Extraction is ready"
 echo " Next: start cropping to 640x480"
